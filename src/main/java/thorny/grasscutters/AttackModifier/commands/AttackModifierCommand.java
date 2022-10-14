9a2e455fa4358ff7a7d862b7ede18ee1e433348e
Yuer-QAQ <1072411694@qq.com>
2022-10-14 22:53:56 +0800

    同步作者0.3版本

--- src/main/java/thorny/grasscutters/AttackModifier/commands/AttackModifierCommand.java
+++ src/main/java/thorny/grasscutters/AttackModifier/commands/AttackModifierCommand.java
@@ -1,7 +1,9 @@
 package thorny.grasscutters.AttackModifier.commands;
 
+import emu.grasscutter.Grasscutter;
 import emu.grasscutter.command.Command;
 import emu.grasscutter.command.CommandHandler;
+import emu.grasscutter.game.avatar.Avatar;
 import emu.grasscutter.game.entity.EntityGadget;
 import emu.grasscutter.game.player.Player;
 import emu.grasscutter.net.proto.VisionTypeOuterClass.VisionType;
@@ -9,17 +11,20 @@ import emu.grasscutter.server.game.GameSession;
 import emu.grasscutter.server.packet.send.PacketSceneEntityDisappearNotify;
 import emu.grasscutter.utils.Position;
 import emu.grasscutter.command.Command.TargetRequirement;
+import emu.grasscutter.data.excels.AvatarSkillDepotData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 // Command usage
-@Command(label = "attack", aliases = "at", usage = "[gadgetId]", targetRequirement = TargetRequirement.NONE)
+@Command(label = "attack", aliases = "at", usage = "on|off \n [gadgetId]", targetRequirement = TargetRequirement.NONE)
 public class AttackModifierCommand implements CommandHandler {
 
     static List<EntityGadget> activeGadgets = new ArrayList<>(); // Current gadgets
     static List<EntityGadget> removeGadgets = new ArrayList<>(); // To be removed gadgets
+
+    public static boolean toAdd = true;
 
     @Override
     public void execute(Player sender, Player targetPlayer, List<String> args) {
@@ -27,14 +32,29 @@ public class AttackModifierCommand implements CommandHandler {
         /*
          * Command usage available to check the gadgets before adding them
          * Just spawns the gadget where the player is standing, given the id
+         * Also allows turning on/off added attacks
          */
 
         // Spawn a gadget at the players location and in the direction faced with /at gadgetId 
         var scene = targetPlayer.getScene();
         var pos = targetPlayer.getPosition();
         var rot = targetPlayer.getRotation();
-        int thing = Integer.parseInt(args.get(0));
+        int thing = 0;
+        String state = "on";
         
+        try {
+            state = args.get(0);
+        } catch (Exception e) {
+            thing = Integer.parseInt(args.get(0));
+        }
+        
+        // Change whether added attacks should be on or not
+        if(state.equals("off")){
+            toAdd = false;
+        }
+        if(state.equals("on")){
+            toAdd = true;
+        }
 
         EntityGadget entity = new EntityGadget(scene, thing, pos, rot);
         scene.addEntity(entity);
@@ -43,38 +63,66 @@ public class AttackModifierCommand implements CommandHandler {
 
     public static void addAttack(GameSession session, int skillId){
 
-        int addedAttack = 0; // Default of no gadget
-        String attType = ""; // Default of no type
-        
-        // Currently will only damage the player
-        switch (skillId) { // For Raiden
-            case 10521: // LeiShen-A
-                addedAttack = 42906105;
-                attType = "basic";
-                break;
-            case 10522: // LeiShen-E
-                addedAttack = 42906108;
-                attType = "elemental";
-                break;
-            case 10525: // LeiShen-Q
-                addedAttack = 42906119;
-                attType = "burst";
-                break;
-             case 10271: // NingGuang-A
-                addedAttack = 41027007;
-                attType = "burst";
-                break;
-             case 10591: // XiaoLu-A
-                addedAttack = 41022001;
-                attType = "burst";
-                break;
-             case 10481: // YanFei-A
-                addedAttack = 41016001;
-                attType = "burst";
-                break;
-            default:
-                // Do nothing
-                break;
+        if(toAdd){
+
+        int addedAttack = -1; // Default of no gadget
+        int usedAttack = -1; // Default of no attack
+
+        // Get avatar info
+        Avatar avatar = session.getPlayer().getTeamManager().getCurrentAvatarEntity().getAvatar();
+        AvatarSkillDepotData skillDepot = avatar.getSkillDepot();
+        int avatarId = avatar.getAvatarId();
+
+        // Check what skill type was used
+        if (skillId == (skillDepot.getSkills().get(0))) {
+            usedAttack = 0;
+        }
+        if (skillId == (skillDepot.getSkills().get(1))) {
+            usedAttack = 1;
+        }
+        if (skillId == (skillDepot.getEnergySkill())) {
+            usedAttack = 2;
+        }
+
+        // Use attack for specific avatar
+        switch (avatarId) {
+            default -> usedAttack = -1;
+            case 10000052 -> { // Raiden
+                switch (usedAttack) {
+                    default -> addedAttack = -1;
+                    case 0 -> addedAttack = 42906105; // Normal attack
+                    case 1 -> addedAttack = 42906108; // Elemental skill
+                    case 2 -> addedAttack = 42906119; // Burst
+                }
+            }
+            // Dummy example case for additional avatars
+            case 10000063 -> { // Shenhe
+                switch (usedAttack) {
+                    default -> addedAttack = -1;
+                    case 0 -> addedAttack = 41069031; // Normal attack
+                    case 1 -> addedAttack = 41069021; // Elemental skill
+                    case 2 -> addedAttack = 41022001; // Burst
+                }
+            } // Shenhe
+
+                        case 10000027 -> { // Niangguang
+                switch (usedAttack) {
+                    default -> addedAttack = -1;
+                    case 0 -> addedAttack = 41027007; // Normal attack
+                }
+            } // Niangguang
+                        case 10000048 -> { // YanFei
+                switch (usedAttack) {
+                    default -> addedAttack = -1;
+                    case 0 -> addedAttack = 41016001; // Normal attack
+                }
+            } // YanFei
+                        case 10000059 -> { // XiaoLu
+                switch (usedAttack) {
+                    default -> addedAttack = -1;
+                    case 0 -> addedAttack = 41022001; // Normal attack
+                }
+            } // XiaoLu
         }
 
         // Get position
@@ -88,7 +136,7 @@ public class AttackModifierCommand implements CommandHandler {
         Position target = new Position(pos);
 
         // Only change gadget pos for basic attack
-        if(attType.equals("basic")){
+        if(usedAttack == 0){
             target.addX((float) (r * Math.sin(Math.PI/180 * angle)));
             target.addZ((float) (r * Math.cos(Math.PI/180 * angle)));
         }
@@ -102,6 +150,7 @@ public class AttackModifierCommand implements CommandHandler {
             att.setGroupId(currTime);
             
             activeGadgets.add(att);
+            
             // Try to make it not hurt self
             scene.addEntity(att);
             att.setFightProperty(2001, 0);
@@ -126,5 +175,6 @@ public class AttackModifierCommand implements CommandHandler {
             activeGadgets.removeAll(removeGadgets);
             removeGadgets.clear();
         } // if
-    } // addAttack
+    } // if toAdd
+} // addAttack
 } // AttackModifierCommand
